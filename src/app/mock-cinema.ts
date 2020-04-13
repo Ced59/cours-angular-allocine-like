4318f1776bed2ccf9d17b2b82e35000ee4abc4e3
Cedric <c.caudron59@gmail.com>
2020-04-13 10:18:08 +0200

    first commit

---
+++ src/app/mock-cinema.ts
@@ -0,0 +1,74 @@
+export const
+  cinemas = [
+    {
+      id: 0,
+      nom: 'Dumbledore Ciné',
+      adresse: '5 rue Harry Potter',
+      ville: 'Poudlard',
+      image: 'Exposition-Harry-Potter-Cite-Cinema-1.jpg',
+      filmsAffiche: [
+        {
+          id: 0,
+          titre: 'Les Simpsons: le film'
+        },
+        {
+          id: 1,
+          titre: 'D.A.R.Y.L.'
+        },
+        {
+          id: 2,
+          titre: 'La gloire de mon père'
+        },
+        {
+          id: 3,
+          titre: 'Ghostbusters'
+        }
+      ]
+    },
+    {
+      id: 1,
+      nom: 'Simpsons Ciné',
+      adresse: '742 Evergreen Terrace',
+      ville: 'Springfield',
+      image: 'Aztec_Theater.png',
+      filmsAffiche: [
+        {
+          id: 1,
+          titre: 'D.A.R.Y.L.'
+        },
+        {
+          id: 2,
+          titre: 'La gloire de mon père'
+        },
+        {
+          id: 3,
+          titre: 'Ghostbusters'
+        }
+      ]
+    },
+    {
+      id: 2,
+      nom: 'CIA Ciné',
+      adresse: '66 rue Big Brother',
+      ville: 'Langley',
+      image: 'unnamed.jpg',
+      filmsAffiche: [
+        {
+          id: 0,
+          titre: 'Les Simpsons: le film'
+        },
+        {
+          id: 1,
+          titre: 'D.A.R.Y.L.'
+        },
+        {
+          id: 2,
+          titre: 'La gloire de mon père'
+        }
+      ]
+    }
+  ];
+
+
+
+
